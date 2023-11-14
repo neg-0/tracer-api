deb05f69937532b3cf9eb64cf0981e5926a7693e
Dustin Stringer <dustin@negativezeroinc.com>
2023-11-13 21:53:52 -0800

    Add new tables for services and deployments

--- migrations/20231111204536_init.js
+++ migrations/20231111204536_init.js
@@ -15,7 +15,28 @@ exports.up = function (knex) {
       table.increments('id');
       table.string('name');
       table.string('key').unique();
+      table.string('address');
       table.integer('user_id').unsigned().references('id').inTable('users');
+      table.timestamps();
+    })
+    .createTable('services', table => {
+      table.increments('id');
+      table.string('name');
+      table.string('language');
+      table.string('technology');
+      table.string('framework');
+      table.string('database');
+      table.string('subdirectory');
+      table.string('runtime');
+      table.string('type');
+      table.timestamps();
+    })
+    .createTable('deployments', table => {
+      table.increments('id');
+      table.string('address');
+      table.integer('service_id').unsigned().references('id').inTable('services');
+      table.time('deployed_at').defaultTo(knex.fn.now());
+      table.time('last_alive').defaultTo(knex.fn.now());
       table.timestamps();
     });
 };
@@ -25,5 +46,5 @@ exports.up = function (knex) {
  * @returns { Promise<void> }
  */
 exports.down = function (knex) {
-  return knex.schema.dropTable('apps').dropTable('users');
+  return knex.schema.dropTable('apps').dropTable('users').dropTable('services').dropTable('deployments');
 };
